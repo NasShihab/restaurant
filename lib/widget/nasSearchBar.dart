20ca17a335320eafd0a197778e8fca8d1b6010c3
NAS <nas.shihab92@gmail.com>
2022-05-29 23:43:14 +0600

    Search box problem all done ! + Demo

--- lib/widget/nasSearchBar.dart
+++ lib/widget/nasSearchBar.dart
@@ -15,7 +15,7 @@ class _nasSearchBarState extends State<nasSearchBar> {
       padding: EdgeInsetsDirectional.only(start: 10, end: 10),
       child: CupertinoSearchTextField(
         decoration: BoxDecoration(border: Border.all(color: Colors.green)),
-        placeholder: 'Nas Shihab',
+        placeholder: 'Nas Shihab s',
         suffixMode: OverlayVisibilityMode.always,
         suffixIcon: Icon(CupertinoIcons.search),
       ),
