a07919962d09d229ab6a6c4c3ea39e87e6dabeb0
NAS <nas.shihab92@gmail.com>
2022-05-30 00:43:22 +0600

    Testing Srch Done

--- lib/widget/nasSearchBar.dart
+++ lib/widget/nasSearchBar.dart
@@ -15,7 +15,7 @@ class _nasSearchBarState extends State<nasSearchBar> {
       padding: EdgeInsetsDirectional.only(start: 10, end: 10),
       child: CupertinoTextField(
         decoration: BoxDecoration(border: Border.all(color: Colors.green)),
-        placeholder: 'Bijoy Search',
+        placeholder: 'Testing Search',
         suffix: Icon(CupertinoIcons.search),
       ),
     );
