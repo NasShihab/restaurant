9bebbb8c2669b4f59152c40bb6c1d6dbf821a7bb
NAS <nas.shihab92@gmail.com>
2022-05-30 00:43:48 +0600

    Bijoy Srch Done

--- lib/widget/nasSearchBar.dart
+++ lib/widget/nasSearchBar.dart
@@ -16,7 +16,7 @@ class _nasSearchBarState extends State<nasSearchBar> {
       child: CupertinoTextField(
         padding: EdgeInsets.all(12),
         decoration: BoxDecoration(border: Border.all(color: Colors.green)),
-        placeholder: 'Nas Shihab',
+        placeholder: 'Bijoy Search',
         suffixMode: OverlayVisibilityMode.always,
         suffix: Icon(CupertinoIcons.search),
       ),
